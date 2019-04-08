c4bce0aa518744e531d70785c6a58b89a16828e0
Justin <jbtcao@gmail.com>
2019-04-07 21:32:57 -0500

    Moved fake address gen to service.

--- Pages/Directory/Address.cshtml.cs
+++ Pages/Directory/Address.cshtml.cs
@@ -1,30 +1,19 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 using ClinicWeb.Model;
+using ClinicWeb.Services;
 
 namespace ClinicWeb.Pages.Directory
 {
     public class AddressModel : PageModel
     {
-        public List<Address> Addresses { get; set; }
+        public IEnumerable<Address> Addresses { get; set; }
 
         public void OnGet()
         {
-            var fakeAddr = new Address();
-            fakeAddr.AddressId = 0;
-            fakeAddr.City = "Houston";
-            fakeAddr.PostalCode = 77573;
-            fakeAddr.State = "Texas";
-            fakeAddr.StreetAddress = "471 Looneyville Street";
-
-            Addresses = new List<Address>();
-            Addresses.Add(fakeAddr);
+            var repo = new Repo();
+            Addresses = repo.FetchAddresses();
         }
     }
 }
---
+++ Services/Repo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+using ClinicWeb.Model;
+
+namespace ClinicWeb.Services
+{
+    class Repo
+    {
+        public IEnumerable<Address> FetchAddresses()
+        {
+            var result = new List<Address>();
+
+            var fakeAddr = new Address();
+            fakeAddr.AddressId = 0;
+            fakeAddr.City = "Houston";
+            fakeAddr.PostalCode = 77573;
+            fakeAddr.State = "Texas";
+            fakeAddr.StreetAddress = "471 Looneyville Street";
+            result.Add(fakeAddr);
+
+            return result;
+        }
+    }
+}
