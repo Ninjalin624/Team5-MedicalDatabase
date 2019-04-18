7d3cbb9bfc25e7fc32255b4c6baaf9b02a5357cc
ramirocastrejon <47700941+ramirocastrejon@users.noreply.github.com>
2019-04-18 15:47:37 -0500

    accounts for austin p2

--- Pages/Accounts/Create.cshtml.cs
+++ Pages/Accounts/Create.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ClinicWeb.Pages.Account
+namespace ClinicWeb.Pages.Accounts
 {
     public class CreateModel : PageModel
     {
