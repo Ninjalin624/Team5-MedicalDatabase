e67aa231032255e3cf7efb27de28a5edea9666f8
Ninjalin624 <nilanjanc624@gmail.com>
2019-04-17 19:11:24 -0500

    minor changes

--- doctorView.sql
+++ doctorView.sql
@@ -1,18 +1,18 @@
 CREATE OR REPLACE VIEW doctor_list_v AS 
 SELECT
-per.person_id AS 'USER ID',
-doc.doctor_id AS 'DOCTOR ID',
-doc.specialization_id AS 'SPECIALIZATION ID',
-per.first_name AS 'FIRST NAME',
-per.last_name AS 'LAST NAME',
+per.person_id AS 'USER_ID',
+doc.doctor_id AS 'DOCTOR_ID',
+specs.type AS 'SPECIALIZATION',
+per.first_name AS 'FIRST_NAME',
+per.last_name AS 'LAST_NAME',
 per.dob AS 'DOB',
 per.gender AS 'GENDER',
-addr.street_address AS 'STREET ADDRESS',
+addr.street_address AS 'STREET_ADDRESS',
 addr.city AS 'CITY',
 addr.state AS 'STATE',
-addr.postal_code AS 'ZIP CODE',
+addr.postal_code AS 'ZIP_CODE',
 per.phone AS 'PHONE'
-FROM person AS per, doctor AS doc, address AS addr
-WHERE per.person_id = doc.person_id AND per.address_id = addr.address_id;
+FROM person AS per, doctor AS doc, address AS addr, specializations AS specs
+WHERE per.person_id = doc.person_id AND per.address_id = addr.address_id AND doc.specialization_id = specs.specialization_id;
     
 SELECT * FROM doctor_list_v;
--- patientView.sql
+++ patientView.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE VIEW patient_list_v AS 
 SELECT
-per.person_id AS 'USER ID',
-pat.patient_id AS 'PATIENT ID',
-per.first_name AS 'FIRST NAME',
-per.last_name AS 'LAST NAME',
+per.person_id AS 'USER_ID',
+pat.patient_id AS 'PATIENT_ID',
+per.first_name AS 'FIRS_NAME',
+per.last_name AS 'LAST_NAME',
 per.dob AS 'DOB',
 per.gender AS 'GENDER',
-addr.street_address AS 'STREET ADDRESS',
+addr.street_address AS 'STREET_ADDRESS',
 addr.city AS 'CITY',
 addr.state AS 'STATE',
-addr.postal_code AS 'ZIP CODE',
+addr.postal_code AS 'ZIP_CODE',
 per.phone AS 'PHONE'
 FROM person AS per, patient AS pat, address AS addr
 WHERE per.person_id = pat.person_id AND per.address_id = addr.address_id;
