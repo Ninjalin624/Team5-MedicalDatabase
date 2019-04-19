cdaa1fbff61ad709192946f5bb45cee3dc81dadb
Justin <jbtcao@gmail.com>
2019-04-18 21:49:13 -0500

    Added LoginModel.

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClinicWeb.Pages
+{
+    [BindProperties]
+    public class LoginModel : PageModel
+    {
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
