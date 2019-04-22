d63cd7367fe1dc81aba7db43652b7cdeacc164d8
Justin <jbtcao@gmail.com>
2019-04-22 17:41:51 -0500

    Changed reports to not show CSV

--- Pages/Portal/Reports.cshtml.cs
+++ Pages/Portal/Reports.cshtml.cs
@@ -7,6 +7,7 @@ using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using ClinicWeb.Model;
 using ClinicWeb.Util;
 using ClinicWeb.Security;
 using ClinicWeb.Services;
@@ -17,6 +18,9 @@ namespace ClinicWeb.Pages.Portal
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ReportsModel : PageModel
     {
+        public IEnumerable<Address> Addresses { get; set; }
+        public IEnumerable<Patient> Patients { get; set; }
+
         public IActionResult OnGet()
         {
             var authService = new AuthService();
@@ -29,21 +33,25 @@ namespace ClinicWeb.Pages.Portal
 
         public IActionResult OnPost(string dataset, int count)
         {
+            if (!ModelState.IsValid)
+                return Page();
+
             if (dataset == "address")
             {
                 using (var repo = new Repo(ConnectionStrings.Default))
                 {
-                    var addresses = repo.ReadAddresses();
-                    string result = CsvSerializer.SerializeToCsv(addresses);
+                    Addresses = repo.ReadAddresses().Take(count);
 
-                    return File(Encoding.UTF8.GetBytes(result), "text/CSV", "report.csv");
+                    return Page();
                 }
             }
             else if (dataset == "patient")
             {
                 using (var repo = new Repo(ConnectionStrings.Default))
                 {
-                    return File(Encoding.UTF8.GetBytes(repo.ReadPatientsCSV(5)), "text/CSV", "report.csv");
+                    Patients = repo.ReadPatients().Take(count);
+
+                    return Page();
                 }
             }
 
