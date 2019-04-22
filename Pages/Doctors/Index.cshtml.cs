46b857c1e9d4229d9eb35968097fbfa1fc26fc59
Justin <jbtcao@gmail.com>
2019-04-22 15:09:05 -0500

    Added admin access restriction to Doctor viewer

--- Pages/Doctors/Index.cshtml.cs
+++ Pages/Doctors/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ClinicWeb.Model;
 using ClinicWeb.Services;
+using ClinicWeb.Security;
 using MySql.Data.MySqlClient;
 
 namespace ClinicWeb.Pages.Doctors
@@ -15,13 +16,20 @@ namespace ClinicWeb.Pages.Doctors
         [BindProperty]
         public IEnumerable<Doctor> Doctor { get; set; }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            var authService = new AuthService();
+            var account = authService.GetSessionAccount(HttpContext);
+            if (account == null || account.GetAccessLevel() < AccessLevel.Admin)
+                return Unauthorized();
+
             var connStr = "Database=clinicdb; Data Source=team5med-db.mysql.database.azure.com; User Id=Team5DBAdmin@team5med-db; Password=Clinic123";
             using (var repo = new Repo(connStr))
             {
                 Doctor = repo.ReadDoctors();
             }
+
+            return Page();
         }
     }
 }
